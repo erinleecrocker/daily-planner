f3c68f8470be196f58ff9dc9b4857bce6e2c5450
Erin Crocker <erindowns1@me.com>
2020-09-12 19:40:59 -0400

    updated script using forloop to manifest time blocks onto page, got rid of all hard coded html

--- script.js
+++ script.js
@@ -1,16 +1,97 @@
 $(document).ready(function() {
 
-    var userInput = $(".userEventInput")
-    var saveInput = $(".saveBtn")
-    var hourOfDay = $(".hour")  
-    var displayDate = moment().format("dddd, MMMM Do YYYY")
-    
+    var displayDate = moment().format("dddd, MMMM Do YYYY");
+    var currentHour = moment().format("h");
+
+    // create planner object
+    var timeBlockInfo = [
+        {
+            hour: 9,
+            time: "9:00",
+            userEvent: (""),
+        },
+        {
+            hour: 10,
+            time: "10:00",
+            userEvent: (""),
+        },
+        {
+            hour: 11,
+            time: "11:00",
+            userEvent: (""),
+        },
+        {
+            hour: 12,
+            time: "12:00", 
+            userEvent: (""),
+        },
+        {
+            hour: 1,
+            time: "1:00",
+            userEvent: ("")
+        },
+        {
+            hour: 2,
+            time: "2:00",
+            userEvent: ("")
+        },
+        {
+            hour: 3,
+            time: "3:00",
+            userEvent: ("")
+        },
+        {
+            hour: 4,
+            time: "4:00",
+            userEvent: ("")
+        },
+        {
+            hour: 5,
+            time: "5:00",
+            userEvent: ("")
+        }
+
+    ];
+
+    //create time blocks
+    var displayTimeBlocks = function () {
+        
+
+        for (i=0; i<timeBlockInfo.length; i++) {
+            // create elements
+            var timeBlock = $("<div>");
+            var hourEl = $("<p>");
+            var userEventEl = $("<textarea>");
+            var saveEl = $("<button>");
+            //add content
+            timeBlock.addClass("row");
+            hourEl.addClass("hour col-sm-1");
+            userEventEl.addClass("col-sm-10");
+            saveEl.addClass("col-sm-1 saveBtn");
+            hourEl.text(timeBlockInfo[i].time);
+            userEventEl.text(timeBlockInfo[i].userEvent);
+            // append to existing
+            $(".time-block").append(timeBlock);
+            timeBlock.append(hourEl);
+            timeBlock.append(userEventEl);
+            timeBlock.append(saveEl);
+            
+            //console.log(timeBlockInfo[i].hour);
+            //console.log(currentHour);
+        };
+        
+
+    };
     
 
-
+    // format date and set #currentDay w/ current: Day of week, month, day, year
     $("#currentDay").text(displayDate);
 
-    console.log(moment().format("hA"));
+   //add event listener click for submit buttons
+   
+
+
+    displayTimeBlocks(); 
 
 
 
